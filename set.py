ec0004a64d75ef06f85737c5e47d75fdcbfdb2b7
Alshifa <shifashikalgar111213@gmail.com>
2026-07-25 23:51:36 +0530

    set2

--- set.py
+++ set.py
@@ -1,6 +1,26 @@
+# user input and display in set 
 s1= set()
 for val in range(6):
     num = int(input("enter values:"))
     print(num)
     s1.add(num)
     print(s1)
+
+# empty set 
+s1={} 
+print(s1,type(s1))  # o/p is dict not set
+
+s1= set()
+print(s1,type(s1))
+
+# non empty set
+
+a=10
+s= set([a])
+print(s,type(s))
+
+s= set((a))
+print(s,type(s))
+
+s= set((a,))
+print(s,type(s))
